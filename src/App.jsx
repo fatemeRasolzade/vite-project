5c5aa0a2b0cae2c1124e032e70081c40ce399674
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-12-17 17:24:38 +0330

    :sparkles: Created theme context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useReducer } from "react";
 import "./App.css";
+import { useTheme } from "./context/ThemeContext";
 
 const initialState = 0;
 function reducerAction(state, { type, payload }) {
@@ -17,12 +18,19 @@ function reducerAction(state, { type, payload }) {
 
 function App() {
   const [state, dispatch] = useReducer(reducerAction, initialState);
+  const { theme, setTheme } = useTheme();
+
+  console.log(theme, "theme");
 
   return (
     <>
       <h1>{state}</h1>
       <button onClick={() => dispatch({ type: "increase", payload: 1 })}>Increase</button>
       <button onClick={() => dispatch({ type: "decrease", payload: 1 })}>Decrease</button>
+      <p>
+        <input type="checkbox" value={theme} checked={theme === true} onChange={() => setTheme(!theme)} />
+        <label>{theme ? "true" : "false"}</label>
+      </p>
     </>
   );
 }
